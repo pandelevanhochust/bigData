5296323cbaa3c6287ae1ded1b8c454f08e95245f
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-06-15 15:02:24 +0700

    fix API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import FraudDetectionDashboard from "./FraudDetectionDashboard.jsx";
+import TransactionDashboard from "./TransactionDashboard.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
